2acbac25701b43293a49e2ade7b285e495b432ba
Gerard Banasig <tuxrace@yahoo.com>
2020-08-31 13:34:47 +0800

    Feat: add workflows

--- src/components/StockList.tsx
+++ src/components/StockList.tsx
@@ -1,42 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import axios from "axios";
+import { Container, Paper, Typography } from "@material-ui/core";
 
 const StockList = () => {
-    const [stocks, setStocks] = useState([])
+  const [stocks, setStocks] = useState([]);
 
-    useEffect(() => {
-        const getStocks = async () => {
-            const res = await axios.get('http://localhost:3001/api/stocks')
-            if (res) {
-                console.log(res);
-                setStocks(res.data)
-            }
-        }
-        getStocks();
-    }, []);
+  useEffect(() => {
+    const getStocks = async () => {
+      const res = await axios.get("http://localhost:3001/api/stocks");
+      if (res) {
+        console.log(res);
+        setStocks(res.data);
+      }
+    };
+    getStocks();
+  }, []);
 
-    return <Table>
-        <TableHead>
+  return (
+    <Container>
+        <Typography variant="h5"> Stock Price</Typography>
+      <Paper>
+        <Table>
+          <TableHead>
             <TableRow>
-                <TableCell> Symbol </TableCell>
-                <TableCell> Price </TableCell>
-                <TableCell> Trend </TableCell>
+              <TableCell> <strong>Symbol</strong> </TableCell>
+              <TableCell> <strong>Price</strong> </TableCell>
+              <TableCell> <strong>Trend</strong> </TableCell>
             </TableRow>
-        </TableHead>
-        <TableBody>
-            {stocks.map((item: any) => <TableRow>
+          </TableHead>
+          <TableBody>
+            {stocks.map((item: any) => (
+              <TableRow>
                 <TableCell> {item.symbol} </TableCell>
                 <TableCell> {item.price} </TableCell>
                 <TableCell> {item.trend} </TableCell>
-            </TableRow>)}
-            
-        </TableBody>
-    </Table>    
-}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    </Container>
+  );
+};
 
 export default StockList;
